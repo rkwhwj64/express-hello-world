1ffdbc8e21cd7de5dc7b1ecf9117627fdff87df2
rkwhwj64 <hwilliamsjr@gmail.com>
2025-07-01 19:47:32 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ const port = process.env.PORT || 3000;
 // Tesla OAuth info
 const clientId = '3d28e98a-a85d-4ed8-8d49-1f6b48c86e55';
 const redirectUri = 'https://henry-magic-mirror.onrender.com/callback';
-const scope = 'openid email offline_access';
+const scope = 'openid%20offline_access';
 
 app.get('/', (req, res) => {
-  const url = `https://auth.tesla.com/oauth2/v3/authorize?client_id=${clientId}&scope=openid%20email%20offline_access&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&state=magicmirror`;
-  res.redirect(url);
+  const authUrl = `https://auth.tesla.com/oauth2/v3/authorize?client_id=${clientId}&scope=${scope}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&state=magicmirror`;
+  console.log("Redirecting to:", authUrl);
+  res.redirect(authUrl);
 });
+
 
 app.get('/callback', (req, res) => {
   const { code, state } = req.query;
